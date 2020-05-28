d65ddd22a4a1ec0d0d8135bd3abbb13a78faa2b5
tipok <me@tipok.pro>
2020-05-28 21:08:13 +0200

    fix lint error

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"flag"
-	"github.com/prometheus/common/version"
 	"log"
 	"net/http"
 	"os"
@@ -12,13 +11,11 @@ import (
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/version"
+
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	netatmo "github.com/tipok/netatmo_exporter/netatmo-api"
 )
-
-func init() {
-	prometheus.MustRegister(version.NewCollector("netatmo_exporter"))
-}
 
 func main() {
 	var clientID string
@@ -48,6 +45,8 @@ func main() {
 	if password == "" {
 		log.Fatal("Netatmo password has to be provided.")
 	}
+
+	prometheus.MustRegister(version.NewCollector("netatmo_exporter"))
 
 	cnf := &netatmo.Config{
 		ClientID:     clientID,
